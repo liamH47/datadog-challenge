53e7149a69e695882d9c0d0c09a44588ab95ddb7
liamH47 <liamhanafee@gmail.com>
2021-06-01 14:09:59 -0500

    integrated docker container

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 
-// const tracer = require('dd-trace').init({
-//   env: 'node-hooks',
-//   analytics: 'true'
-// })
+const tracer = require('dd-trace').init({
+  env: 'node-hooks',
+  analytics: 'true'
+});
+const axios = require('axios');
+const faker = require('faker');
+const _ = require('lodash');
+const express = require('express');
+const app = express();
+const port = 4000;
 
-// const express = require('express');
-// const app = express();
-// const port = 4000;
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
 
-// app.listen(port, function(){
-//   console.log(`app listening on port ${port}`);
-// })
-
-const express = require('express')
-const app = express()
-const port = 4000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Increment a counter.
+dogstatsd.increment('page.views')
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`app listening on port ${port}`);
 })
+
+app.get('/', (req, res) => {
+  res.send({
+    "firstName": faker.name.firstName
+  })
+})
+
+
+app.get('/address', (req, res) => {
+  const count = req.query.count;
+  if (!count) {
+    return res
+      .status(400)
+      .send({ errorMsg: 'count query parameter is missing.' });
+  }
+  res.send(
+    _.times(count, () => {
+      const address = faker.address;
+      return {
+        country: address.country(),
+        city: address.city(),
+        state: address.state(),
+        zipCode: address.zipCode(),
+        latitude: address.latitude(),
+        longitude: address.longitude()
+      };
+    })
+  );
+});
